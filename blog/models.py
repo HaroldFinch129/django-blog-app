6efd635ecc7694aa07abc4bea820adb738ed4737
Harold <m.h.gamze@gmail.com>
2021-10-27 00:02:13 +0300

    set the post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields.files import ImageField
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    image = models.ImageField(upload_to="profiles/%Y", default = "default.jpg")
+    publish_date = models.DateTimeField(auto_now_add = True)
+    last_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20)
+    slug = models.SlugField(
+        verbose_name='slug',
+        allow_unicode=True,
+        max_length=255,
+        help_text=("The name of the page as it will appear in URLs e.g http://domain.com/blog/[my-slug]/")
+    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
