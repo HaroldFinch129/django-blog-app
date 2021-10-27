9556bc49328e32dad5bb1dc7559be602a6ed7a42
Harold <m.h.gamze@gmail.com>
2021-10-27 11:14:06 +0300

    set again order of models

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Comment, Like, PostView, Profile, Category, Post
-
+from .models import Profile,Category,Post,PostView,Comment,Like
 # Register your models here.
 
 admin.site.register(Profile)
