e7fda1204fbc95c7440f6d10ef0d3b710a1e284c
Harold <m.h.gamze@gmail.com>
2021-10-30 00:29:13 +0300

    debug and password

--- main/settings.py
+++ main/settings.py
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# DATABASES={
-#    'default':{
-#       'ENGINE':'django.db.backends.postgresql_psycopg2',
-#       'NAME': 'blog_app',  #db propertiesden
-#       'USER':'postgres',     #db propertiesden
-#       'PASSWORD':'1Pgadmin2!',  # postgresql password
-#       'HOST':'localhost',   
-#       'PORT':'5432',            
-#    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME': 'blog_app',  #db propertiesden
+      'USER':'postgres',     #db propertiesden
+      'PASSWORD':config('1Pgadmin2!'),  # postgresql password
+      'HOST':'localhost',   
+      'PORT':'5432',            
+   }
+}
 
 
 # Password validation
